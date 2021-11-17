bb4c2c8b3c59338520f2a91377c068578eaac8bc
Aminul Islam <aminulds61@gmail.com>
2021-11-17 13:11:17 +0600

    complete the Hulu clone

--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,19 @@ import {
 } from "@heroicons/react/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import { useRouter } from "next/router";
 
 function Header() {
+  const router = useRouter();
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <span onClick={() => router.push(`/`)}>
+          <HeaderItem title="HOME" Icon={HomeIcon} />
+        </span>
+        <span onClick={() => router.push(`/?genre=fetchTrending`)}>
+          <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        </span>
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
